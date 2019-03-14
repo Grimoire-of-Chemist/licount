b84962b8bcaf6ff8394c4a911c6494dc18c732c6
“LF112” <lf@lf112.net>
2019-03-14 19:04:04 +0800

    Create licount.js

---
+++ licount.js
@@ -0,0 +1,17 @@
+// Javascript licount - Simple lightweight counting component.
+// // Released under GNU General Public License v3.0 Opensourced. (https://github.com/LF112/licount/)
+// Author @LF112 (https://github.com/lf112)
+// Copyright reservation is required.
+
+let licount = function( Config ){
+
+    console.log('\n %c 🧷licount %c https://github.com/LF112/licount %c BY%c LF112  \n\n', 'color: #ffffff; background: rgb(0, 145, 228); padding:5px 0;', 'background:rgba(197, 197, 197, 0.89); padding:5px 0;', 'color: #ffffff; background: rgba(49, 49, 49, 0.85); padding:5px 0;', 'color: rgb(0, 145, 228); background: rgba(49, 49, 49, 0.85); padding:5px 0;');
+
+    return new Promise( function() {
+
+        // Main
+
+        //Coding more...
+
+    });
+};
