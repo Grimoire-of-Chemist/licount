2d3c0f6acc321d66253a646d2ad2c1ccb5d2f04c
“LF112” <lf@lf112.net>
2019-03-14 21:24:18 +0800

    Create the specified count

--- licount.js
+++ licount.js
@@ -3,7 +3,7 @@
 // Author @LF112 (https://github.com/lf112)
 // Copyright reservation is required.
 
-let licount = function( Config ){
+let LiCount_Lock = true,LiCount_Lock2 = true,LiCount_Main,LiCount_On, licount = function( Config ){
 
     console.log('\n %c 🧷licount %c https://github.com/LF112/licount %c BY%c LF112  \n\n', 'color: #ffffff; background: rgb(0, 145, 228); padding:5px 0;', 'background:rgba(197, 197, 197, 0.89); padding:5px 0;', 'color: #ffffff; background: rgba(49, 49, 49, 0.85); padding:5px 0;', 'color: rgb(0, 145, 228); background: rgba(49, 49, 49, 0.85); padding:5px 0;');
 
@@ -13,59 +13,105 @@ let licount = function( Config ){
 
         //Verify or create LeanCloud Javascript SDK.
         try {
-            if (typeof(AV.version) == 'undefined') console.log('「LiCount」Successfully loaded the LeanCloud SDK, but the version is unknown.');
+            if (typeof(AV.version) == 'undefined') { console.log('「LiCount」Successfully loaded the LeanCloud SDK, but the version is unknown.'); LiCount_Lock2 = false; On() }
                 else console.log('「LiCount」Successfully loaded LeanCloud SDK V' + AV.version);
         }catch (e) {
             if (e) {
                 console.log('「LiCount」LeanCloud Javascript SDK not found...' + '\n「LiCount」Now, try loading the LeanCloud Javascript SDK.');
 
                 const Load_LC_SDK = document.createElement('script');
-                Load_LC_SDK.setAttribute('src', 'https://cdn.jsdelivr.net/npm/leancloud-storage@3.12.0/dist/av-min.js');
+                Load_LC_SDK.setAttribute('src', 'https://cdn1.lncld.net/static/js/av-core-mini-0.6.1.js');
                 document.getElementsByTagName('body')[0].appendChild(Load_LC_SDK);
-                Load_LC_SDK.onload = function() { console.log('「LiCount」LeanCloud is loaded!' + '\n「LiCount」LeanCloud SDK V' + AV.version) }
+                Load_LC_SDK.onload = function() {
+                    console.log('「LiCount」LeanCloud is loaded!' + '\n「LiCount」LeanCloud SDK V' + AV.version);
+                    LiCount_Lock2 = false;
+                    On()
+                }
             } else console.log('「LiCount」Unknown error: ' + e);
         }
 
         //Judging and create configuration.
-        if (typeof(Config) != 'undefined') {
-            if (typeof(Config.APPID) != 'undefined' && typeof(Config.APPKEY) != 'undefined'){
+        function On() {
+            if (typeof (Config) != 'undefined') {
+                if (typeof (Config.APPID) != 'undefined' && typeof (Config.APPKEY) != 'undefined') {
 
-                AV.initialize(Config.APPID, Config.APPKEY);
-                LiCount_Main = new AV.Query(AV.Object.extend('LiCount'));
-                LiCount_Lock = false;
+                    if (!LiCount_Lock2) {
+                        AV.initialize(Config.APPID, Config.APPKEY);
+                        LiCount_On = AV.Object.extend('LiCount');
+                        LiCount_Main = new AV.Query(LiCount_On);
+                        LiCount_Lock = false
+                    }else console.log('「LiCount」LeanCloud is not loaded!');
 
-            } else console.log('「LiCount」APPID or APPKEY are not defined!');
-        } else console.log('「LiCount」Being ready to go :)');
+                } else console.log('「LiCount」APPID or APPKEY are not defined!');
+            } else console.log('「LiCount」Being ready to go :)');
+        }
 
         //Coding more...
 
     });
-}, LiCount_Lock = true,LiCount_Main, licount_Show = function ( ID ) {
+}, licount_Show = function ( ID ) {
     return new Promise(function () {
 
         //Get the specified count.
 
          if (!LiCount_Lock) {
 
-             LiCount_Main.equalTo('ID',ID);
+             LiCount_Main.equalTo('ID', ID);
              LiCount_Main.find({
-                 success: function(results) {
-                     if (results.length != 0) {
-                         for (var i = 0; i < results.length; i++) {
-                             var object = results[i];
-                             return object.get('count');
-                         }
+                 success: function( Results ) {
+                     if ( Results.length != 0 ) {
+                         for ( var i = 0; i < Results.length; i++ ) return Results[i].get('count');
                      } else {
                          console.log('「LiCount」' + ID + ' has no data!');
                          return 0
                      }
                  },
-                 error: function(object, error) {
-                     console.log("「LiCount」LeanCloud Javascript SDK Error: " + error.code + " " + error.message);
+                 error: function( object, error ) {
+                     console.log('「LiCount」LeanCloud Javascript SDK Error: ' + error.code + ' ' + error.message);
                  }
              });
 
          } else console.log('「LiCount」"licount_Show()" call failed, because LiCount is not initialized, please first "licount({APPID:\'_Your APPID_\',\'_Your APPKEY_\'});"');
+
+    });
+}, licount_Add = function ( ID ) {
+    return new Promise(function () {
+
+        //Create the specified count.
+
+        if (!LiCount_Lock) {
+
+            LiCount_Main.equalTo('ID', ID);
+            LiCount_Main.find({
+                success: function( Results ) {
+                    if ( Results.length > 0 ) {
+                        var licount_ = Results[0];
+                        licount_.fetchWhenSave(true);
+                        licount_.increment('count');
+                        licount_.save(null, {
+                            success: function( ResultGet ) {
+                                return ResultGet.get('count');
+                            },
+                            error: function( ResultGet, error ) { console.log('「LiCount」LeanCloud Javascript SDK Failed to save Visitor num, with error message: ' + error.message) }
+                        });
+                    } else {
+                        var LiCount_On_To = new LiCount_On();
+                        LiCount_On_To.set('ID', ID);
+                        LiCount_On_To.set('count', 1);
+                        LiCount_On_To.save(null, {
+                            success: function( ResultGet ) {
+                                return ResultGet.get('count');
+                            },
+                            error: function( ResultGet, error ) { console.log('「LiCount」LeanCloud Javascript SDK Failed to create!') }
+                        });
+                    }
+                },
+                error: function(error) {
+                    console.log('「LiCount」LeanCloud Javascript SDK Error: ' + error.code + ' ' + error.message);
+                }
+            });
+
+        } else console.log('「LiCount」"licount_Show()" call failed, because LiCount is not initialized, please first "licount({APPID:\'_Your APPID_\',\'_Your APPKEY_\'});"');
 
     });
 };
